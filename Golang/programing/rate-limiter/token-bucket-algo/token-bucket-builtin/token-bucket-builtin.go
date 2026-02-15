acfc99bef441afec376842f4476c544a384598d0
Ankush-Hegde <ankushhegde44@gmail.com>
2026-02-15 22:01:42 +0530

    //

--- Golang/programing/rate-limiter/token-bucket-algo/token-bucket-builtin/token-bucket-builtin.go
+++ Golang/programing/rate-limiter/token-bucket-algo/token-bucket-builtin/token-bucket-builtin.go
@@ -1,23 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"time"
+// import (
+// 	"fmt"
+// 	"time"
 
-	"golang.org/x/time/rate"
-)
+// 	"golang.org/x/time/rate" // can be installed only in go.mod file
+// )
 
-func main() {
-	limiter := rate.NewLimiter(2, 5)
-	// 2 events per second
-	// burst capacity 5
+// func main() {
+// 	limiter := rate.NewLimiter(2, 5)
+// 	// 2 events per second
+// 	// burst capacity 5
 
-	for i := 0; i < 10; i++ {
-		if limiter.Allow() {
-			fmt.Println("Allowed")
-		} else {
-			fmt.Println("Blocked")
-		}
-		time.Sleep(300 * time.Millisecond)
-	}
-}
+// 	for i := 0; i < 10; i++ {
+// 		if limiter.Allow() {
+// 			fmt.Println("Allowed")
+// 		} else {
+// 			fmt.Println("Blocked")
+// 		}
+// 		time.Sleep(300 * time.Millisecond)
+// 	}
+// }
